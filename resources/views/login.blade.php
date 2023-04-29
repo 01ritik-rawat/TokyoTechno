088446334bcea8e4cd4645da88b34207d7972dba
01ritik-rawat <01ritikrawat@gmail.com>
2023-04-29 22:22:02 +0530

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+@extends('master')
+@section('content')
+
+
+<h1>
+    Login Page
+</h1>
+
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4 mx-auto">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+gi
+        </div>
+
+    </div>
+
+</div>
+
+
+@endsection
