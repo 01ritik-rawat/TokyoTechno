a6d8feedf64647ad61ae60dafdc649836f25b37a
01ritik-rawat <01ritikrawat@gmail.com>
2023-04-30 16:11:18 +0530

    fixed the slider issue

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,33 +6,41 @@
     <div class="row">
 
 
-        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-            </ol>
-
-            <div class="carousel-inner">
-
-                @foreach ($products as $key => $item)
-                <div class="carousel-item  {{ $key == 0 ? 'active' : '' }}">
-                    <img src="{{$item['gallery']}}" class="d-block w-100" alt=" alt text">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                </div>
-                @endforeach
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <!-- slide indicators -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
+
+            <!-- slides -->
+            <div class="carousel-inner">
+                @foreach ($products as $item )
+                
+                    <div class="carousel-item {{$item['id']==1 ? 'active' : '' }}">
+                        <img src="{{$item['gallery']}}" class="d-block w-100" alt="Images of Products">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>{{$item['name']}}</h5>
+                            <p>{{$item['description']}}</p>
+                        </div>
+                    </div>
+                    
+                @endforeach
+
+
+            </div>
+
+            <!-- navigation button -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
+                <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
+                <span class="visually-hidden">Next</span>
             </button>
+            
         </div>
 
 
